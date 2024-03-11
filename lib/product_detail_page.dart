07b8ec3fdbd731508bb6ae55067ea1baf1a506d1
Yana Mulyana <yanamyn15@gmail.com>
2024-03-11 17:09:38 +0700

    add singlescrollview

--- lib/product_detail_page.dart
+++ lib/product_detail_page.dart
@@ -40,9 +40,9 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             Navigator.pop(context);
           },
         ),
-        title: const Text(
-          "Detail",
-          style: TextStyle(color: Colors.black),
+        title: Text(
+          widget.product.name,
+          style: const TextStyle(color: Colors.black),
         ),
         actions: const [
           Padding(
@@ -53,109 +53,116 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
       ),
       body: Column(
         children: [
-          Text(
-            widget.product.name,
-            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
-          ),
-          Center(
-            child: Hero(
-              tag: widget.product.image,
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.45,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(widget.product.image),
-                      fit: BoxFit.contain),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.grey[200],
-            padding: const EdgeInsets.only(left: 20, top: 20),
-            child: Row(
-              children: [
-                Text(
-                  widget.product.price,
-                  style: const TextStyle(
-                      color: Colors.green,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700),
-                ),
-                Text(
-                  "/${widget.product.quantity}",
-                  style: const TextStyle(
-                      color: Colors.green,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400),
-                ),
-              ],
-            ),
-          ),
           Expanded(
-            child: Container(
-              color: Colors.grey[200],
-              padding: const EdgeInsets.all(20),
-              child: Text(
-                widget.product.description,
-                style: const TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                ),
-                textAlign: TextAlign.justify,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 7,
-              ),
-            ),
-          ),
-          Container(
-            height: 100,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            alignment: Alignment.bottomCenter,
-            color: Colors.grey,
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  IconButton(
-                    onPressed: removeItem,
-                    icon: const Icon(Icons.remove_circle_outline),
-                    color: Colors.white,
-                  ),
-                  Text(
-                    totalCount.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700,
+                  Center(
+                    child: Hero(
+                      tag: widget.product.image,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(widget.product.image),
+                              fit: BoxFit.contain),
+                        ),
+                      ),
                     ),
-                  ),
-                  IconButton(
-                    onPressed: addItem,
-                    icon: const Icon(Icons.add_circle_outline),
-                    color: Colors.white,
                   ),
                   Container(
-                    width: 190,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(25),
-                      ),
-                      color: Colors.green,
+                    color: Colors.grey[200],
+                    padding: const EdgeInsets.only(left: 20, top: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          widget.product.price,
+                          style: const TextStyle(
+                              color: Colors.green,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          "/${widget.product.quantity}",
+                          style: const TextStyle(
+                              color: Colors.green,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
                     ),
-                    child: const Center(
-                      child: Text(
-                        "Pesan",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      widget.product.description,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
                       ),
+                      textAlign: TextAlign.justify,
                     ),
                   ),
                 ],
               ),
+            ),
+          ),
+          Container(
+            height: 80,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.bottomCenter,
+            color: Colors.grey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 8,
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: removeItem,
+                        icon: const Icon(Icons.remove_circle_outline),
+                        color: Colors.white,
+                      ),
+                      Text(
+                        totalCount.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: addItem,
+                        icon: const Icon(Icons.add_circle_outline),
+                        color: Colors.white,
+                      ),
+                      Container(
+                        width: 190,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                          color: Colors.green,
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Pesan",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
